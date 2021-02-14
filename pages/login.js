484c2eb17e647b49fadd9529b12c190a6140acc2
Allister Rampenthal <allisterrampenthal@gmail.com>
2021-02-13 22:17:55 -0600

    Update login.js

--- pages/login.js
+++ pages/login.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import { Button, Divider, Form, Grid, Segment } from 'semantic-ui-react'
 import styles from '../styles/Home.module.css'
 
 export default function Login() {
@@ -8,7 +9,23 @@ export default function Login() {
                 <title>HFB Inventory | Login</title>
             </Head>
 
-            <div>
+            <div className={styles.container}>
+                <Form>
+                    <Form.Input
+                        icon='user'
+                        iconPosition='left'
+                        label='Username'
+                        placeholder='Username'
+                    />
+                    <Form.Input
+                        icon='lock'
+                        iconPosition='left'
+                        label='Password'
+                        type='password'
+                    />
+
+                    <Button content='Login' primary />
+                </Form>
 
             </div>
         </>
