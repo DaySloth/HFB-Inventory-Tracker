199807ed268bbed1c401f58c91ae91e7f540a1bc
Allister Rampenthal <allisterrampenthal@gmail.com>
2021-02-13 21:50:54 -0600

    Create login.js

---
+++ pages/login.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Login() {
+    return (
+        <>
+            <Head>
+                <title>HFB Inventory | Login</title>
+            </Head>
+
+            <div>
+
+            </div>
+        </>
+    )
+}
