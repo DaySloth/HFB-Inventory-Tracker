f23db5985cf881365d796cf4efd3c865e8bfa350
Allister Rampenthal <allisterrampenthal@gmail.com>
2021-02-16 17:11:24 -0600

    Update header.js

--- components/header.js
+++ components/header.js
@@ -1,21 +1,31 @@
-import { Dropdown, Menu, Grid } from 'semantic-ui-react'
-import Link from 'next/Link'
-import styles from '../styles/Home.module.css'
+import { Dropdown, Menu, Grid } from "semantic-ui-react";
+import Link from "next/Link";
+import styles from "../styles/Home.module.css";
 
 export default function Header() {
-
     const options = [
         { key: 1, text: <Link href="/">View Warehouse</Link>, value: 1 },
-        { key: 2, text: <Link href="/parts/add-a-part">Add a Part</Link>, value: 2 },
-        { key: 3, text: <Link href="/parts/edit-a-part">Edit a Part</Link>, value: 3 },
-    ]
+        {
+            key: 2,
+            text: <Link href="/parts/add-a-part">Add a Part</Link>,
+            value: 2,
+        },
+        {
+            key: 3,
+            text: <Link href="/parts/edit-a-part">Edit a Part</Link>,
+            value: 3,
+        },
+    ];
 
     return (
         <nav className={styles.mainNav}>
-            <Grid columns='equal' className={styles.centerText}>
+            <Grid columns="equal" className={styles.centerText}>
                 <Grid.Row>
                     <Grid.Column>
-                        <h2>Logo</h2>
+                        <img
+                            src="/Logo/Circle/HomeForeverBaths-Logo-White.PNG"
+                            className={styles.headerLogo}
+                        />
                     </Grid.Column>
                     <Grid.Column>
                         <h2>Inventory Management</h2>
@@ -23,15 +33,22 @@ export default function Header() {
                     <Grid.Column>
                         <div className={styles.floatLeft}>
                             <Menu compact>
-                                <Dropdown text='Menu' options={options} simple item />
+                                <Dropdown
+                                    text="Menu"
+                                    options={options}
+                                    simple
+                                    item
+                                />
                             </Menu>
                         </div>
                         <div className={styles.floatRight}>
-                            <h3>Hello Paige, <span>Logout</span></h3>
+                            <h3>
+                                Hello Paige, <span>Logout</span>
+                            </h3>
                         </div>
                     </Grid.Column>
                 </Grid.Row>
             </Grid>
         </nav>
-    )
+    );
 }
