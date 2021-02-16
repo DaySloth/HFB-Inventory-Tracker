8eb93043fea3d1ad22554bbf66eb354534d92257
Allister Rampenthal <allisterrampenthal@gmail.com>
2021-02-15 22:17:43 -0600

    Update login.js

--- pages/login.js
+++ pages/login.js
@@ -24,7 +24,7 @@ export default function Login() {
                         type='password'
                     />
 
-                    <Button content='Login' primary />
+                    <Button content='Login' onClick={() => window.location.href = "/"} primary />
                 </Form>
 
             </div>
