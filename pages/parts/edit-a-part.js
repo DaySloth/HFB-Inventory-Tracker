28332976dde80764f2952c0290feaa74fb50cefc
Allister Rampenthal <allisterrampenthal@gmail.com>
2021-02-16 16:54:26 -0600

    adding jsx for editing a part

--- pages/parts/edit-a-part.js
+++ pages/parts/edit-a-part.js
@@ -1,12 +1,15 @@
-import Head from 'next/head'
-import NavHeader from '../../components/header.js'
-import { Header, Icon } from 'semantic-ui-react'
-import styles from '../../styles/Home.module.css'
+import Head from "next/head";
+import NavHeader from "../../components/header.js";
+import { Header, Icon, Grid, Input, Select, Button } from "semantic-ui-react";
+import styles from "../../styles/Home.module.css";
 
 export default function EditaPart() {
+    const categoryOptions = [
+        { key: 1, value: "accessories", text: "Accessories" },
+    ];
+
     return (
         <>
-
             <Head>
                 <title>HFB Inventory | Edit a Part</title>
             </Head>
@@ -14,13 +17,77 @@ export default function EditaPart() {
             <NavHeader />
 
             <div className={styles.center}>
-                <Header as='h2' icon>
-                    <Icon name='edit outline' />
+                <Header as="h2" icon>
+                    <Icon name="edit outline" />
                     Edit a Part
                 </Header>
             </div>
             <hr />
+            <div className={styles.container}>
+                <label>Search for part:</label>
+                <br />
+                <Input
+                    icon="search"
+                    placeholder="Search..."
+                    className={styles.halfWidth}
+                />
+                <Button inverted color="blue">
+                    Search
+                </Button>
+            </div>
 
+            {/* <div className={styles.container}>
+                <Grid columns="equal">
+                    <Grid.Row>
+                        <Grid.Column>
+                            <label>Brand:</label>
+                            <Input className={styles.fullWidth} />
+                        </Grid.Column>
+                        <Grid.Column>
+                            <label>Part/Model #:</label>
+                            <Input className={styles.fullWidth} />
+                        </Grid.Column>
+                        <Grid.Column>
+                            <label>Part Name:</label>
+                            <Input className={styles.fullWidth} />
+                        </Grid.Column>
+                    </Grid.Row>
+                    <Grid.Row>
+                        <Grid.Column>
+                            <label>Serial:</label>
+                            <Input className={styles.fullWidth} />
+                        </Grid.Column>
+                        <Grid.Column>
+                            <label>Color:</label>
+                            <Input className={styles.fullWidth} />
+                        </Grid.Column>
+                    </Grid.Row>
+                    <Grid.Row>
+                        <Grid.Column>
+                            <label>Category:</label>
+                            <br />
+                            <Select
+                                options={categoryOptions}
+                                className={styles.fullWidth}
+                            />
+                        </Grid.Column>
+                        <Grid.Column>
+                            <div>
+                                <label>Quantity:</label>
+                                <Input
+                                    type="number"
+                                    className={styles.fullWidth}
+                                />
+                            </div>
+                        </Grid.Column>
+                    </Grid.Row>
+                </Grid>
+                <div className={styles.centerButton}>
+                    <Button inverted color="blue">
+                        Save Edit
+                    </Button>
+                </div>
+            </div> */}
         </>
-    )
+    );
 }
