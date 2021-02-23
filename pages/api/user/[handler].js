f752102bbd95e820f039d952f166739030ea803c
Allister Rampenthal <allisterrampenthal@gmail.com>
2021-02-23 16:21:08 -0600

    getting mongodb working for calls

--- pages/api/user/[handler].js
+++ pages/api/user/[handler].js
@@ -1,40 +1,35 @@
-import mongoose from "mongoose";
 import bcrypt from "bcrypt";
-import dbUser from "../../../models/user.js";
-
-mongoose.connect(
-    "mongodb+srv://arampenthal:Batmanisc00l@database.1cd32.mongodb.net/InventoryTracker?retryWrites=true&w=majority",
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    }
-);
+import { connectToDatabase } from "../../../util/mongodb";
 
 export default async (req, res) => {
+    const { db } = await connectToDatabase();
+    const Users = await db.collection("users");
+
     const {
         query: { handler },
     } = req;
+
     const { username, password } = req.body;
 
     switch (handler) {
         case "login":
-            const user = await dbUser.findOne({ email: username });
+            const { documents: user } = await Users.find({});
             console.log("user_:", user);
             if (user) {
                 console.log("found the user");
             } else {
                 console.log("couldn't find user");
             }
-            mongoose.connection.close();
             break;
+
         case "create":
             console.log("hitting create");
-            mongoose.connection.close();
             break;
+
         case "delete":
             console.log("hitting delete");
-            mongoose.connection.close();
             break;
+
         default:
             console.log("hitting default");
             mongoose.connection.close();
