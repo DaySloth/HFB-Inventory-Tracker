3fb92a6b1df37196cdfdb150c7f382d1d2bc5cc6
Allister <allisterrampenthal@gmail.com>
2021-03-27 19:41:35 -0500

    formatting for user edits

--- components/header.js
+++ components/header.js
@@ -5,66 +5,63 @@ import styles from "../styles/Home.module.css";
 import { signOut, useSession } from "next-auth/client";
 
 export default function Header() {
-    const [session, loading] = useSession();
-    const router = useRouter();
-    // const options = [
-    //     { key: 1, text: "View Users", value: "/" },
-    //     {
-    //         key: 2,
-    //         text: "Add a part",
-    //         value: "/parts/add-a-part",
-    //     },
-    //     {
-    //         key: 3,
-    //         text: "Edit a part",
-    //         value: "/parts/edit-a-part",
-    //     },
-    // ];
+  const [session, loading] = useSession();
+  const router = useRouter();
+  const options = [
+      { key: 1, text: "View Users", value: "/" },
+      {
+          key: 2,
+          text: "Add a User",
+          value: "/users/add-a-user",
+      },
+      {
+          key: 3,
+          text: "Edit Admin Account",
+          value: "/users/edit-admin",
+      },
+  ];
 
-    return (
-        <nav className={styles.mainNav}>
-            <Grid columns="equal" className={styles.centerText}>
-                <Grid.Row>
-                    <Grid.Column>
-                        <Link href="/">
-                            <img
-                                src="/HomeForeverBaths-Logo-White.png"
-                                alt="Home Forever Baths logo"
-                                className={styles.headerLogo}
-                            />
-                        </Link>
-                    </Grid.Column>
-                    <Grid.Column>
-                        <h2>HFB Mobile Manager</h2>
-                    </Grid.Column>
-                    <Grid.Column>
-                        {/* <div className={styles.floatLeft}>
-                            <Menu compact>
-                                <Dropdown
-                                    text="Menu"
-                                    simple
-                                    item
-                                    options={options}
-                                    onChange={(e, { value }) => {
-                                        router.push(value);
-                                    }}
-                                />
-                            </Menu>
-                        </div> */}
-                        <div className={styles.floatRight}>
-                            <h3>
-                                Hello {session.user.name},{" "}
-                                <span
-                                    className={styles.logout}
-                                    onClick={() => signOut()}
-                                >
-                                    Logout
-                                </span>
-                            </h3>
-                        </div>
-                    </Grid.Column>
-                </Grid.Row>
-            </Grid>
-        </nav>
-    );
+  return (
+    <nav className={styles.mainNav}>
+      <Grid columns="equal" className={styles.centerText}>
+        <Grid.Row>
+          <Grid.Column>
+            <Link href="/">
+              <img
+                src="/HomeForeverBaths-Logo-White.png"
+                alt="Home Forever Baths logo"
+                className={styles.headerLogo}
+              />
+            </Link>
+          </Grid.Column>
+          <Grid.Column>
+            <h2>HFB Mobile Manager</h2>
+          </Grid.Column>
+          <Grid.Column>
+            <div className={styles.floatLeft}>
+              <Menu compact>
+                <Dropdown
+                  text="Menu"
+                  simple
+                  item
+                  options={options}
+                  onChange={(e, { value }) => {
+                    router.push(value);
+                  }}
+                />
+              </Menu>
+            </div>
+            <div className={styles.floatRight}>
+              <h3>
+                Hello {session.user.name},{" "}
+                <span className={styles.logout} onClick={() => signOut()}>
+                  Logout
+                </span>
+              </h3>
+            </div>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </nav>
+  );
 }
