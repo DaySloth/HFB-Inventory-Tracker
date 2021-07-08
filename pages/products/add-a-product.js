37c07fdac32d1ca64e50b74f1cb0fb954f80afe5
Allister Rampenthal <allisterrampenthal@gmail.com>
2021-07-08 14:01:42 -0500

    getting sql setup

--- pages/products/add-a-product.js
+++ pages/products/add-a-product.js
@@ -12,6 +12,9 @@ import {
   Segment,
   Label,
   List,
+  Form,
+  Checkbox,
+  Grid,
 } from "semantic-ui-react";
 import { useSession } from "next-auth/client";
 import React, { useEffect, useState } from "react";
@@ -51,72 +54,30 @@ export default function AddAProduct({ products }) {
           </div>
 
           <hr />
-          {products ? (
+          {true ? (
             <>
-              {/* <div className={styles.container}>
-                <Input
-                  icon="search"
-                  placeholder="Search..."
-                  className={styles.fullWidth}
-                />
-              </div> */}
-
               <div className={styles.tableContainer}>
-                <Table celled>
-                  <Table.Header>
-                    <Table.Row>
-                      <Table.HeaderCell>First Name</Table.HeaderCell>
-                      <Table.HeaderCell>Last Name</Table.HeaderCell>
-                      <Table.HeaderCell>Email</Table.HeaderCell>
-                      <Table.HeaderCell>Username</Table.HeaderCell>
-                      <Table.HeaderCell>Password</Table.HeaderCell>
-                      <Table.HeaderCell textAlign="center">
-                        Web Access
-                      </Table.HeaderCell>
-                      <Table.HeaderCell textAlign="center">
-                        Edit
-                      </Table.HeaderCell>
-                    </Table.Row>
-                  </Table.Header>
-                  <Table.Body>
-                    {Object.keys(users).map((user) => (
-                      <>
-                        <Table.Row key={users[user].email}>
-                          <Table.Cell>{users[user].first_name}</Table.Cell>
-                          <Table.Cell>{users[user].last_name}</Table.Cell>
-                          <Table.Cell>{users[user].email}</Table.Cell>
-                          <Table.Cell>{user}</Table.Cell>
-                          <Table.Cell>{"**********"}</Table.Cell>
-                          <Table.Cell textAlign="center">
-                            {users[user].hasManagerAccess ? "Yes" : "No"}
-                          </Table.Cell>
-                          <Table.Cell textAlign="center">
-                            <Icon
-                              name="edit"
-                              className={styles.iconHover}
-                              onClick={() => {
-                                Router.push(
-                                  `/users/edit/${users[
-                                    user
-                                  ].first_name.toLowerCase()}${users[
-                                    user
-                                  ].last_name.toLowerCase()}`
-                                );
-                              }}
-                            />
-                          </Table.Cell>
-                        </Table.Row>
-                      </>
-                    ))}
-                  </Table.Body>
-                </Table>
+                <Form>
+                  <Form.Group widths="equal">
+                    <Form.Input
+                      fluid
+                      label="First name"
+                      placeholder="First name"
+                    />
+                    <Form.Input
+                      fluid
+                      label="Last name"
+                      placeholder="Last name"
+                    />
+                  </Form.Group>
+                </Form>
               </div>
             </>
           ) : (
             <Segment placeholder>
               <Header icon>
-                <Icon name="bath" />
-                Form not ready yet..
+                <Icon name="user secret" />
+                Sneaky sneaks..
               </Header>
               {/* <Button primary href="/users/add-a-user">
                 Add a User
